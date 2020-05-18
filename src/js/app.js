dfbe61c5b5a1d1d298478f6235ed1bd45048577b
GustavoEspíndola <espindolage@gmail.com>
2020-05-18 17:35:34 -0400

    update
    Fb Chat

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 		
-	// Click Scroll
+
 	$( "a.scrollLink" ).click(function( event ) {
 	event.preventDefault();
 		$("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
 	});
 
-});
-
 // $('body').hide();
+});

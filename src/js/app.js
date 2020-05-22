8a0a7925029beba5170009cff99d04b1423db58e
GustavoEspíndola <espindolage@gmail.com>
2020-05-22 02:30:20 -0400

    fix fb chat

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 
-
 $(document).ready(function(){
-	$('.fb').append('<div id="fb-root"></div> <script> window.fbAsyncInit = function() { FB.init({ xfbml            : true, version          : "v7.0" }); }; (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src =  "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"; fjs.parentNode.insertBefore(js, fjs); }(document, "script ",  "facebook-jssdk ""));</script> <div class="fb-customerchat" attribution=setup_tool page_id="475738506254897"theme_color="#568B88"> </div>');
 		AOS.init({
 		  // Global settings:
 		  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
